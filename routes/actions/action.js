5e53ab1a103e2bb30376053db72a6e1647310ef9
vy3191 <venkyafhs@gmail.com>
2019-12-15 21:52:16 -0500

    Completed-inserting actions

--- routes/actions/action.js
+++ routes/actions/action.js
@@ -6,7 +6,19 @@ router.get("/:actionId",getActionsById, async (req,res) => {
       res.json(req.action)
 });
 
+router.post("/", validateActions, async (req,res) => {
+     try {
+      const payload = {
+        project_id: req.params.id,
+        description: req.body.description,
+        notes: req.body.notes
+     }
+      res.status(201).json(await actions.insert(payload));
+     }catch(error) {
+          res.status(500).json({msg:error})
+     }
 
+});
 
 function getActionsById(req,res,next) {
     actions.get(req.params.actionId)
@@ -19,6 +31,14 @@ function getActionsById(req,res,next) {
           .catch(err => {
             res.status(500).json({msg:`Something went wrong--server error`});
           })
+}
+
+function validateActions(req,res,next) {
+   const { description, notes} = req.body;
+   if(!description) res.status(400).json({msg:`Description is missing`});
+   if(!notes) res.status(400).json({msg:`Notes are missing in your action`});
+   if(!notes && !description) res.status(400).json({msg:`Action is missing`});
+   next();
 }
 
 module.exports = router;
