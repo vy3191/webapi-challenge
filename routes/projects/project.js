c7f00a1cca25d834b56b45e7e86332988f70a426
vy3191 <venkyafhs@gmail.com>
2019-12-15 20:37:15 -0500

    Done- post method on projects

--- routes/projects/project.js
+++ routes/projects/project.js
@@ -9,7 +9,29 @@ router.get("/", async (req,res) => {
     } catch(error) {
        res.status(500).json({msg:error})
     }
-})
+});
 
+router.post("/", validateProject, async(req,res) => {
+     try {
+      const playload = {
+        name: req.body.name,
+        description:req.body.description
+     }
+       res.status(200).json( await projects.insert(playload));
+
+     }catch(error) {
+        
+     }
+
+});
+
+
+function validateProject(req,res,next) {
+    const { name, description } = req.body;
+    if(!name) res.status(400).json({msg:`Name is missing`});
+    if(!description) res.status(400).json({msg:`Description is missing`});
+    if(!name && !description) res.status(400).json({msg:`project details are missing`});
+    next();
+}
 
 module.exports = router;
